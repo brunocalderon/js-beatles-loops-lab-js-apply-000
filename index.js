a9ad0bfac4a7ed007c8ec5184084aed1d270ba65
Bruno Calderon <b@brunocalderon.com>
2019-06-21 08:28:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ function johnLennonFacts(facts) {
 
 function iLoveTheBeatles(index) {
   do {
-    return ['I love the Beatles!'];
+    return ["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"];
   }
   while (index === 7);
+  do {
+    return []
+  }
 }
